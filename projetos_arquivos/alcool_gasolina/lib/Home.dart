0f278fc659c31bb05a792d30818c563c6e4304fd
danieldfr <daniel.dfr@outlook.com>
2022-09-20 22:37:18 -0300

    UPD Implementação do método principal
    - Implementação do método para informar qual a melhor opção de abastecimento.
    - Método adicional para limpar campos após calculo.

--- projetos_arquivos/alcool_gasolina/lib/Home.dart
+++ projetos_arquivos/alcool_gasolina/lib/Home.dart
@@ -11,6 +11,35 @@ class _HomeState extends State<Home> {
 
   TextEditingController _controllerAlcool = TextEditingController();
   TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "";
+
+  void _calcular(){
+
+    double? precoAlcool = double.tryParse( _controllerAlcool.text );
+    double? precoGasolina = double.tryParse( _controllerGasolina.text );
+
+    if( precoAlcool == null || precoGasolina == null){
+      setState((){
+        _textoResultado = "Número inválido, digite números maiores que 0 e utilizando (.)";
+      });
+    }else{
+      if( (precoAlcool / precoGasolina) >= 0.7 ){
+        setState(() {
+          _textoResultado = "Melhor abastecer com gasolina!";
+        });
+      }else{
+        setState(() {
+          _textoResultado = "Melhor abastecer com álcool!";
+        });
+      }
+      //_limparCampos();
+    }
+  }
+
+  void _limparCampos(){
+    _controllerGasolina.text = "";
+    _controllerAlcool.text = "";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,13 +99,13 @@ class _HomeState extends State<Home> {
                         fontSize: 20
                     ),
                   ),
-                  onPressed: (){},
+                  onPressed: _calcular,
                 ),
               ),
               Padding(
                 padding: EdgeInsets.only(top: 20),
                 child: Text(
-                  "Resultado",
+                  _textoResultado,
                   style: TextStyle(
                     fontSize: 22,
                     fontWeight: FontWeight.bold,
