079686c216378fddb1de70ce05d388a4317038c0
danieldfr <daniel.dfr@outlook.com>
2022-09-24 16:30:43 -0300

    ADD componente RadioButton
    Implementação de como utilizar o componente RadioButton. Utilizando o projeto entrada_dados.

---
+++ projetos_arquivos/entrada_dados/lib/EntradaRadioButton.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class EntradaRadioButton extends StatefulWidget {
+  const EntradaRadioButton({Key? key}) : super(key: key);
+
+  @override
+  State<EntradaRadioButton> createState() => _EntradaRadioButtonState();
+}
+
+class _EntradaRadioButtonState extends State<EntradaRadioButton> {
+
+  String? _escolhaUsuario;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Entrada de dados"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+
+            RadioListTile(
+              title: Text("Masculino"),
+                value: "m",
+                groupValue: _escolhaUsuario,
+                onChanged: (escolha){
+                  setState(() {
+                    _escolhaUsuario = escolha.toString();
+                  });
+                  print("Resultado: " + escolha.toString());
+                }
+            ),
+            RadioListTile(
+                title: Text("Feminino"),
+                value: "f",
+                groupValue: _escolhaUsuario,
+                onChanged: (escolha){
+                  setState(() {
+                    _escolhaUsuario = escolha.toString();
+                  });
+                  print("Resultado: " + escolha.toString());
+                }
+            ),
+            RadioListTile(
+                title: Text("Teste"),
+                value: "t",
+                groupValue: _escolhaUsuario,
+                onChanged: (escolha){
+                  setState(() {
+                    _escolhaUsuario = escolha.toString();
+                  });
+                  print("Resultado: " + escolha.toString());
+                }
+            ),
+
+            RaisedButton(
+              child: Text(
+                "Salvar",
+                style: TextStyle(
+                    fontSize: 20
+                ),
+              ),
+              onPressed: (){
+                print("Resultado: " + _escolhaUsuario.toString());
+              },
+            ),
+
+
+            /*
+            Text("Masculino"),
+            Radio(
+                value: "m",
+                groupValue: _escolhaUsuario,
+                onChanged: (escolha){
+                  setState(() {
+                    _escolhaUsuario = escolha.toString();
+                  });
+                  print("Resultado: " + escolha.toString());
+                }
+            ),
+            Text("Feminino"),
+            Radio(
+                value: "f",
+                groupValue: _escolhaUsuario,
+                onChanged: (escolha){
+                  setState(() {
+                    _escolhaUsuario = escolha.toString();
+                  });
+                  print("Resultado: " + escolha.toString());
+                }
+            )
+            */
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- projetos_arquivos/entrada_dados/lib/main.dart
+++ projetos_arquivos/entrada_dados/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:entrada_dados/CampoTexto.dart';
+import 'package:entrada_dados/EntradaRadioButton.dart';
 import 'package:flutter/material.dart';
 import 'package:entrada_dados/EntradaCheckbox.dart';
 void main(){
   runApp(
     MaterialApp(
-      home: EntradaCheckbox(),
+      home: EntradaRadioButton(),
+      //home: EntradaCheckbox(),
       //home: CampoTexto(),
     )
   );
