1bbc56a402b619a9684a464d1df0b6bc277da32a
danieldfr <daniel.dfr@outlook.com>
2022-10-07 10:14:17 -0300

    ADD projeto cara_ou_coroa
    Criação do projeto com exemplo de navegação entre telas e passar dados entre telas

---
+++ projetos_arquivos/cara_ou_coroa/lib/Jogar.dart
@@ -0,0 +1,46 @@
+import 'package:cara_ou_coroa/resultado.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Jogar extends StatefulWidget {
+  const Jogar({Key? key}) : super(key: key);
+
+  @override
+  State<Jogar> createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+
+  void _exibirResultado(){
+
+    var itens = ["cara", "coroa"];
+    var numero = Random().nextInt( itens.length );
+    var resultado = itens[numero];
+
+    Navigator.push(
+      context, 
+      MaterialPageRoute(
+        builder: (builder) => Resultado(resultado)  
+      ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("images/botao_jogar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
