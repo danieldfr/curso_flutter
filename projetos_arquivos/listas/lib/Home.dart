2ee3a3083916adbd60a3c01a5b14f31fac7af44a
danieldfr <daniel.dfr@outlook.com>
2022-10-27 15:15:06 -0300

    Updata Home.dart
    Update da classe Home.dart e implementação da funcionalidade AlertDialog

--- projetos_arquivos/listas/lib/Home.dart
+++ projetos_arquivos/listas/lib/Home.dart
@@ -13,7 +13,7 @@ class _HomeState extends State<Home> {
 
   void _carregarItens(){
 
-    List _itens = [];
+    _itens = [];
 
     for(int i=0; i<=10; i++){
       
@@ -41,9 +41,48 @@ class _HomeState extends State<Home> {
         child: ListView.builder(
           itemCount: _itens.length,
           itemBuilder: (context, indice){
-            print("item ${indice}");
+            
+            //print("item ${ _itens[indice].toString() }");
 
             return ListTile(
+              onTap: (){
+                showDialog(
+                  context: context, 
+                  builder: (context){
+                    return AlertDialog(
+                      title: Text( _itens[indice]["titulo"] ),
+                      titlePadding: EdgeInsets.all(20),
+                      titleTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.orange,
+                      ),
+
+                      content: Text( _itens[indice]["descricao"] ),
+                      actions: <Widget>[
+                        ElevatedButton(
+                          onPressed: (){
+                            print("Selecionado SIM");
+                            Navigator.pop(context);
+                          },
+                          child: Text("Sim"),                          
+                        ),
+                        ElevatedButton(
+                          onPressed: (){
+                            print("Selecionado NÃO");
+                            Navigator.pop(context);
+                          },
+                          child: Text("Não"),                          
+                        )
+                      ],
+
+                    );
+                  }
+                );
+              },
+              /*onLongPress: (){
+
+              },*/
+
               title: Text( _itens[indice]["titulo"] ),
               subtitle: Text( _itens[indice]["descricao"]  ),
             );
